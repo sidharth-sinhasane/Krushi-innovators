9b87fbbf176c23c86cb3d6d082dd27fb3245c035
sidharth_sinhasane <sidharth.sinhasane@gmail.com>
2024-02-17 20:29:23 +0530

    server is updated to handle n images

--- server/app.py
+++ server/app.py
@@ -24,34 +24,36 @@ def upload_file():
     if 'file' not in request.files:
         return jsonify({'error': 'No file part in the request'}), 400
 
-    file = request.files['file']
+    file= request.files.getlist('file')
+    no_img=len(file)
+    avg_confi=0
 
-    # If the user does not select a file, the browser submits an empty file without a filename
-    if file.filename == '':
-        return jsonify({'error': 'No selected file'}), 400
-
+    for i in range(0,no_img):
+        if file[i].filename == '':
+            return jsonify({'error': 'No selected file'}), 400
     # Check if the file is allowed based on its extension
-    allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
-    if '.' not in file.filename or file.filename.rsplit('.', 1)[1].lower() not in allowed_extensions:
-        return jsonify({'error': 'Unsupported file format'}), 400
+        allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
+        if '.' not in file[i].filename or file.filename.rsplit('.', 1)[1].lower() not in allowed_extensions:
+            return jsonify({'error': 'Unsupported file format'}), 400
 
     # Save the uploaded file to the upload folder
-    file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-    file.save(file_path)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], file[i].filename)
+        file[i].save(file_path)
     
-    results = model.predict(file_path)
-    result = results[0]
-    boxes = result.boxes
-    cls_np = boxes.cls.cpu().numpy().astype(int)
-    conf_np = boxes.conf.cpu().numpy()
-    prediction = None
-    if len(cls_np)>0:
-        label = cls_np[0]
-        confidence = conf_np[0]
-        prediction = {
-            'name':model.names[label],
-            'confidence':int(confidence*100)
-        }
+        results = model.predict(file_path)
+        result = results[0]
+        boxes = result.boxes
+        cls_np = boxes.cls.cpu().numpy().astype(int)
+        conf_np = boxes.conf.cpu().numpy()
+        prediction = None
+        if len(cls_np)>0:
+            label = cls_np[0]
+            confidence = conf_np[0]
+            avg_confi+=int(confidence*100)
+            prediction = {
+            'name':model.names[label]
+            }
+    prediction['confidence']=avg_confi/no_img
     print(prediction)
 
     return jsonify({'message': 'File uploaded successfully', 'file_path': file_path, 'prediction':prediction}), 200
